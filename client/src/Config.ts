3774c149f95759a82013c8d383eb5a69e5754923
FadiBunni <FadiBunni@users.noreply.github.com>
2021-03-07 19:01:58 +0100

    added LICENSE.md file

--- client/src/Config.ts
+++ client/src/Config.ts
@@ -5,6 +5,7 @@ export default class Config {
     public static showInspector = false;
     // useNetworking can be disabled if you want to only develop client-side
     public static useNetworking = true;
+    // quick way to target any level made in blender, see public/assets/scenes.
     public static levelName = "Level1.babylon";
     // this address currently hosts the game
     public static socketAddressProduction = "wss://kzclimbing.herokuapp.com";
