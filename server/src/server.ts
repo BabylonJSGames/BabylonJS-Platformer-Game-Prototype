6480bda59b2806aaa6cdff8c9bafc9acc1e37469
Fadi Bunni <FadiBunni@users.noreply.github.com>
2023-04-28 01:13:01 +0200

    updated server
    again trying with CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,12 +12,13 @@ const port = Number(process.env.PORT || 8081);
 const app = express();
 
 app.use(cors());
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 app.use(express.json())
 
 
